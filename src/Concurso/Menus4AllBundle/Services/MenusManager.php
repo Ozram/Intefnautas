5ad2d0de286a566556cd7840c198eeffe62f044e
Ozram <ozram@Anima-00.(none)>
2012-10-28 01:47:15 +0200

    acabado el manager de menus

--- src/Concurso/Menus4AllBundle/Services/MenusManager.php
+++ src/Concurso/Menus4AllBundle/Services/MenusManager.php
@@ -15,10 +15,23 @@ class MenusManager {
 
     public function createMenu($json) {
         $data = json_decode($json, true);
-        $menu = new Menu();
+        $session = $this->getRequest()->getSession();
+        $idUsuario = $session->getId();
         try {
             $menu->setNombre($data['nombre']);
             $menu->setDescripcion($data['descripcion']);
+            $menu->setUsuario($idUsuario);
+            $menu->setTipoMenu($data['tipo']);
+            //se recorren las ids de recetas y se añaden al menu
+            if (empty($data['recetas'])) {
+                $resultado['statusCode'] = 500;
+                $resultado['errores']['QueMeEstasContainer'] = 'El menú debe tener al menos una receta asociada';
+                return $resultado;
+            }
+            foreach ($data['recetas'] as $idReceta) {
+                $receta = $this->em->getRepository('ConcursoMenus4AllBundle:Receta')->find($idReceta);
+                $menu->addReceta($receta);
+            }
             $errors = $this->validator->validate($menu);
             if (count($errors) > 0) {
                 $resultado['statusCode'] = 422;
@@ -47,6 +60,25 @@ class MenusManager {
             $listaMenu['id'] = $menu->getId();
             $listaMenu['nombre'] = $menu->getNombre();
             $listaMenu['descripcion'] = $menu->getDescripcion();
+            $recetasMenu = $menu->getRecetas();
+            foreach ($recetasMenu as $i => $recetaMenu) {
+                $listaMenu['recetas'][$i]['id'] = $recetaMenu->getId();
+                $listaMenu['recetas'][$i]['nombre'] = $recetaMenu->getNombre();
+                $listaMenu['recetas'][$i]['n_personas'] = $recetaMenu->getNPersonas();
+            }
+            $valoracionesMenu = $menu->getValoraciones();
+            $puntuacionTotal = 0;
+            foreach ($valoracionesMenu as $i => $valoracionMenu) {
+                $listaMenu['valoraciones'][$i]['id'] = $valoracionMenu->getId();
+                $listaMenu['valoraciones'][$i]['idAutor'] = $valoracionMenu->getUsuario()->getId();
+                $listaMenu['valoraciones'][$i]['nombreAutor'] = $valoracionMenu->getUsuario()->getNombre();
+                $listaMenu['valoraciones'][$i]['puntuacion'] = $valoracionMenu->getPuntuacion();
+                $puntuacionTotal = $valoracionMenu->getPuntuacion() + $puntuacionTotal;
+                $listaMenu['valoraciones'][$i]['comentario'] = $valoracionMenu->getComentario();
+            }
+            $listaMenu['valoracionMedia'] = $puntuacionTotal / ($i + 1);
+            $listaMenu['tipo'] = $menu->getTipoMenu()->getNombre();
+            $listaMenu['autor'] = $menu->getUsuario()->getNombre();
             $resultado['listaMenu'] = $listaMenu;
             $resultado['statusCode'] = 200;
         } catch (\ErrorException $mapexc) {
@@ -61,10 +93,29 @@ class MenusManager {
         try {
             $menus = $this->em->getRepository('ConcursoMenus4AllBundle:Menu')->findAll();
             $listaMenus = array();
-            foreach ($menus as $i => $menu) {
-                $listaMenus[$i]['id'] = $menu->getId();
-                $listaMenus[$i]['nombre'] = $menu->getNombre();
-                $listaMenus[$i]['descripcion'] = $menu->getDescripcion();
+            foreach ($menus as $n => $menu) {
+                $listaMenus[$n]['id'] = $menu->getId();
+                $listaMenus[$n]['nombre'] = $menu->getNombre();
+                $listaMenus[$n]['descripcion'] = $menu->getDescripcion();
+                $recetasMenu = $menu->getRecetas();
+                foreach ($recetasMenu as $i => $recetaMenu) {
+                    $listaMenus[$n]['recetas'][$i]['id'] = $recetaMenu->getId();
+                    $listaMenus[$n]['recetas'][$i]['nombre'] = $recetaMenu->getNombre();
+                    $listaMenus[$n]['recetas'][$i]['n_personas'] = $recetaMenu->getNPersonas();
+                }
+                $valoracionesMenu = $menu->getValoraciones();
+                $puntuacionTotal = 0;
+                foreach ($valoracionesMenu as $i => $valoracionMenu) {
+                    $listaMenus[$n]['valoraciones'][$i]['id'] = $valoracionMenu->getId();
+                    $listaMenus[$n]['valoraciones'][$i]['idAutor'] = $valoracionMenu->getUsuario()->getId();
+                    $listaMenus[$n]['valoraciones'][$i]['nombreAutor'] = $valoracionMenu->getUsuario()->getNombre();
+                    $listaMenus[$n]['valoraciones'][$i]['puntuacion'] = $valoracionMenu->getPuntuacion();
+                    $puntuacionTotal = $valoracionMenu->getPuntuacion() + $puntuacionTotal;
+                    $listaMenus[$n]['valoraciones'][$i]['comentario'] = $valoracionMenu->getComentario();
+                }
+                $listaMenus[$n]['valoracionMedia'] = $puntuacionTotal / ($i + 1);
+                $listaMenus[$n]['tipo'] = $menu->getTipoMenu()->getNombre();
+                $listaMenus[$n]['autor'] = $menu->getUsuario()->getNombre();
             }
             $resultado['listaMenus'] = $listaMenus;
             $resultado['statusCode'] = 200;
@@ -82,6 +133,29 @@ class MenusManager {
             $menu = $this->em->getRepository('ConcursoMenus4AllBundle:Menu')->find($id);
             $menu->setNombre($data['nombre']);
             $menu->setDescripcion($data['descripcion']);
+            if (empty($data['recetas'])) {
+                $resultado['statusCode'] = 500;
+                $resultado['errores']['QueMeEstasContainer'] = 'El menú debe tener al menos una receta asociada';
+                return $resultado;
+            }
+            $recetasDesc = $this->em->getRepository('ConcursoMenus4AllBundle:Menu')->getRecetasDescartadas($data['recetas'], $menu->getId());
+            if (!empty($recetasDesc)) {
+                foreach ($recetasDesc as $recetaDesc) {
+                    $menu->removeReceta($recetaDesc);
+                }
+            }
+            $recetasMenu = $menu->getRecetas();
+            $idsRecetaMenu = array();
+            foreach ($recetasMenu as $recetaMenu) {
+                $idsRecetaMenu[] = $recetaMenu->getId();
+            }
+            foreach ($data['recetas'] as $idReceta) {
+                if (!in_array($idReceta, $idsRecetaMenu)) {
+                    $nuevaRecetaMenu = $menu = $this->em->getRepository('ConcursoMenus4AllBundle:Receta')->find($idReceta);
+                    $menu->addReceta($nuevaRecetaMenu);
+                }
+            }
+            $menu->setTipoMenu($data['tipoMenu']);
             $errors = $this->validator->validate($menu);
             if (count($errors) > 0) {
                 $resultado['statusCode'] = 422;
